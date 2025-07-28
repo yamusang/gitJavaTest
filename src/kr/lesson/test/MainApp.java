8fad0776f46e16cda89308aed7a3a2fa2b302a12
오승현 <hynnyo1@gmail.com>
2025-07-28 14:39:13 +0900

    add file2

--- src/kr/lesson/test/MainApp.java
+++ src/kr/lesson/test/MainApp.java
@@ -4,5 +4,6 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("add file");
+        System.out.println("add file2");
     }
 }
